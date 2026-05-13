156e446b21ac0e9b989773bcad16762721e40307
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2026-05-13 18:52:59 +0300

    feat(users): добавить сущность UserEntity

--- src/domain/users/entities.py
+++ src/domain/users/entities.py
@@ -1,1 +1,27 @@
 # UserEntity (id, email, role)
+from dataclasses import dataclass
+from uuid import UUID
+
+from domain.users.value_objects import Email, UserRole
+
+
+@dataclass
+class UserEntity:
+    """
+    Пользователь
+    """
+
+    id: UUID
+    email: Email
+    password_hash: str
+    role: UserRole
+    telegram_id: str | None = None
+    _version: int = 1
+
+    def link_telegram(self, telegram_id: str) -> None:
+        """
+        Привязка аккаунта telegram.
+        """
+        if self.telegram_id is not None:
+            raise ValueError("Telegram уже привязан")
+        self.telegram_id = telegram_id
