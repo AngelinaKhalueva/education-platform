feec78f26b4c12143227ee21a3a239d137403e42
AngelinaKhalueva <dev.patient.zero@gmail.com>
2026-05-13 21:07:08 +0300

    fix(users): добавить доменные исключения

--- src/domain/users/exceptions.py
+++ src/domain/users/exceptions.py
@@ -1,1 +1,21 @@
 # UserNotFoundError, EmailAlreadyExistsError
+class UserNotFoundError(Exception):
+    """
+    Пользователь не найден.
+    """
+
+    pass
+
+
+class EmailAlreadyExistsError(Exception):
+    """
+    Email уже зарегистрирован в системе.
+    """
+
+
+class TelegramAlreadyLinkedError(Exception):
+    """
+    Telegram уже привязан к другому аккаунту.
+    """
+
+    pass
