34ec65ffd8d71431e49e90cf463cb1d3377ed344
AngelinaKhalueva <dev.patient.zero@gmail.com>
2026-05-20 10:26:16 +0300

    fix(config): отформатировать код с помощью ruff

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
 from django.http import HttpResponse
+from django.urls import include, path
 
 urlpatterns = [
     path("", lambda request: HttpResponse("OK")),
