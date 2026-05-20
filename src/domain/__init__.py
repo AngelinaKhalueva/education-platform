16914af3617a97c92d80b94739a6815dee84107c
AngelinaKhalueva <dev.patient.zero@gmail.com>
2026-05-20 10:33:06 +0300

    fix: добавить __init__

---
+++ src/domain/__init__.py
@@ -0,0 +1,18 @@
+# создание пустого кортежа
+empty_tuple_1 = tuple()
+empty_tuple_2 = ()
+
+# создание кортежа
+numbers_1 = 1, 2, 5, 7
+numbers_2 = ({}, [], "")
+new_numbers_1 = 1,
+new_numbers_2 = (1,)
+
+# получить индекс элемента
+print(numbers_2.index([]))
+# посчитать кол-во элементов
+print(numbers_1.count(1))
+# длина кортежа
+print(len(numbers_1))
+# получить элемент по индексу
+print(new_numbers_2[0])
